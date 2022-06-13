155379d96aefa676513c150f6e866197472560be
JuanScaparro <juanscaparro@gmail.com>
2022-06-13 14:38:21 -0300

    feat: routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -9,6 +10,34 @@ import { StoreComponent } from './pages/store/store.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { FormContactComponent } from './shared/form-contact/form-contact.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'recetas',
+    component: RecipesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'tienda',
+    component: StoreComponent
+  },
+  {
+    path: 'carrito',
+    component: CartComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+]
 
 @NgModule({
   declarations: [
@@ -19,10 +48,12 @@ import { FormContactComponent } from './shared/form-contact/form-contact.compone
     StoreComponent,
     RecipesComponent,
     CartComponent,
-    FormContactComponent
+    FormContactComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( routes )
   ],
   providers: [],
   bootstrap: [AppComponent]
