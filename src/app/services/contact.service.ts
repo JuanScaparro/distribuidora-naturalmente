8feb92c4c4b0f002aa5e069a0aec6ef676a9f699
JuanScaparro <juanscaparro@gmail.com>
2022-06-14 16:58:45 -0300

    feat: implement SweetAlert2

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -3,18 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ContactService {
 
-  private url: string = 'https://dummyjson.com/products/add'
+  private url: string = 'https://dummyjson.com/products/add';
 
   constructor() { }
 
-  public postForm(payload: any) {
-    return fetch(this.url, {
+  public postForm( payload: any ) {
+    return fetch( this.url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
     })
-    .then(res => res.json())
-  }
-}
+    .then(res => res.json());
+  };
+};
--- src/app/shared/form-contact/form-contact.component.ts
+++ src/app/shared/form-contact/form-contact.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 import { ContactService } from '../../services/contact.service';
-import { take } from 'rxjs';
 
 @Component({
   selector: 'app-form-contact',
   templateUrl: './form-contact.component.html',
   styleUrls: ['./form-contact.component.css']
 })
+
 export class FormContactComponent implements OnInit {
 
-  public contactForm: FormGroup
+  public contactForm: FormGroup;
 
-  public result: string = ''
+  public result: Object = {};
 
-  constructor( private contactService: ContactService) {
-    this.contactForm = new FormGroup({})
-  }
+  constructor( private contactService: ContactService ) {
+    this.contactForm = new FormGroup({});
+  };
 
   ngOnInit(): void {
     this.buildForm();
-  }
+  };
 
   private buildForm() {
     this.contactForm = new FormGroup({
@@ -28,23 +30,36 @@ export class FormContactComponent implements OnInit {
       lastName: new FormControl(''),
       email: new FormControl(''),
       textArea: new FormControl(''),
-    })
-  }
+    });
+  };
 
   public onSubmit() {
-    console.log(this.contactForm.value)
-
-    const { firstName, lastName } = this.contactForm.value
-
-    this.contactService.postForm(this.contactForm.value).then(res => {
-      if(res.id) {
-        this.result = `Gracias ${firstName} ${lastName} por contactarnos!`
+    const { firstName, lastName } = this.contactForm.value;
+    this.contactService.postForm( this.contactForm.value ).then( response => {
+      if( response.id ) {
+        this.result = Swal.fire({
+                        position: 'bottom-end',
+                        icon: 'success',
+                        title: `Gracias ${ firstName } ${ lastName } por contactarnos!`,
+                        showConfirmButton: false,
+                        timer: 2500,
+                      })
       } else {
-        this.result = `Oooops! ${firstName} ${lastName} lo lamentamos, pero hubo un error. Intentalo nuevamente!`
+        this.result = Swal.fire({
+                        position: 'bottom-end',
+                        icon: 'error',
+                        title: `Oooops! ${ firstName } ${ lastName } lo lamentamos, pero hubo un error. Intentalo nuevamente!`,
+                        showConfirmButton: false,
+                        timer: 2500,
+                      })
       }
-    }).catch( e =>  this.result = `Oooops! Nuestros servidores son una mierda`  )
+    }).catch( err =>  this.result = Swal.fire({
+                                      position: 'bottom-end',
+                                      icon: 'error',
+                                      title: `Oooops! Nuestros servidores son una mierda`,
+                                      showConfirmButton: false,
+                                      timer: 2500
+                                    }) );
+  };
 
-
-  }
-
-}
+};
